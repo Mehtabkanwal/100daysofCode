7d1d1b8283a9fe43561613f527624e7c3a18476d
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-03-22 20:37:41 +0500

    100daysofCode

---
+++ Q62/Q62.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question 62: Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.
+;
+let studentData = {
+    name: "kanwal",
+    age: 28,
+    coursesParticipated: ["GenerativeAI", "Artificial Intelligence", "Metaverse"]
+};
+console.log(studentData);
---
+++ Q63/Q63.js
@@ -0,0 +1,15 @@
+"use strict";
+// Question 63: Shape Shifter: Write a program that can describe either a circle or a rectangle using a special type alias, including properties unique to each shape.
+// Create instances of Circle and Rectangle
+const myCircle = {
+    shape: 'circle',
+    radius: 5,
+};
+const myRectangle = {
+    shape: 'rectangle',
+    width: 8,
+    height: 6,
+};
+// Example usage
+console.log(myCircle);
+console.log(myRectangle);
