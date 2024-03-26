d30abb429baf44d7546fea27d8d865edb4876883
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-03-26 14:39:35 +0500

    100daysofCode

---
+++ Q73/Q73.js
@@ -0,0 +1,10 @@
+"use strict";
+// Question 73: Assigning and Updating Variables: Create a function where you declare a variable using let and assign an initial value. Then, update its value within the same function and log both the initial and updated values.
+function assignUpdateVariable() {
+    let myName = "kanwal";
+    console.log("Initial Value :", myName);
+    myName = "bilal";
+    console.log("Updated Value :", myName);
+}
+;
+assignUpdateVariable();
---
+++ Q73/Q73.ts
@@ -0,0 +1,10 @@
+// Question 73: Assigning and Updating Variables: Create a function where you declare a variable using let and assign an initial value. Then, update its value within the same function and log both the initial and updated values.
+
+
+function assignUpdateVariable () {
+    let myName:string = "kanwal";
+    console.log("Initial Value :" , myName)
+    myName = "bilal";
+    console.log("Updated Value :" ,myName);
+};
+assignUpdateVariable();
