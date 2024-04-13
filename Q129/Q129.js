5e60d1f82b5a06efcee24940f5450bff97f65656
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-04-14 02:41:59 +0500

    100daysofCode

---
+++ Q129/Q129.js
@@ -0,0 +1,14 @@
+// Question 129: Explain how this behaves differently in arrow functions compared to traditional functions.
+var _this = this;
+// Demonstrating 'this' in traditional vs. arrow functions
+var traditionalVsArrow = {
+    value: "traditionalVsArrow value",
+    traditionalFunction: function () {
+        console.log("Traditional function:", this.value);
+    },
+    arrowFunction: function () {
+        console.log("Arrow function:", _this.value);
+    },
+};
+traditionalVsArrow.traditionalFunction();
+traditionalVsArrow.arrowFunction();
